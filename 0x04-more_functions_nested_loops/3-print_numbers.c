6c70889acca52dd93356072f3da990d8a77f7e8f
Freizy <freizykenbrook@gmail.com>
2022-12-16 20:20:58 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints out numbers from 0 - 9
+ * Return: 0
+ */
+int print_numbers(void)
+{
+	char i;
+
+	for (i = 0, i < 10, i++)
+	{
+		_putchar (i);
+	}
+	_putchar (\n);
+}
