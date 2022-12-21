5b70e8bfcdeb7fcfb822485a51b79cf99613c6e1
freizy <freizykenbrook@gmail.com>
2022-12-20 20:33:19 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: char to check
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int s;
+
+	for (s = 0; str[s] != '\0'; s++)
+	if (s % 2 == 0)
+		_putchar(str[s]);
+	_putchar('\n');
