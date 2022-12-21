d77631821964af3b900b5e3eeda7b8a8069d5106
freizy <freizykenbrook@gmail.com>
2022-12-20 20:54:38 -0800

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - print n elem of an arr of ints
+ * @n: number of elements
+ * @a: array
+ * Return: 0;
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+		printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+printf("\n");
+
+}
