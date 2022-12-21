803c3db24623a271a8e60f3b5c6f6eec8e959338
freizy <freizykenbrook@gmail.com>
2022-12-20 19:37:53 -0800

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int
+ * @n: int to update
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Again, Pointers and arrays and strings
