1ce30d6cb0b0dcd6c273f72bd2ee409787e6679d
freizy <freizykenbrook@gmail.com>
2022-12-20 20:10:13 -0800

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * str: string to be printed
+ * Return: the length
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
+
