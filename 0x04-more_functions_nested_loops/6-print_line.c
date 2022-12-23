5259f7a559d80a19b4ef96f0c851f20fbc2333f5
Freizy <freizykenbrook@gmail.com>
2022-12-23 13:49:53 -0800

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times the line should be printed
+ * Return: 0
+ */
+void print_line(int n)
+{
+	char i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar (_);
+	}
+}
