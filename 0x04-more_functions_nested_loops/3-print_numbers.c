53ea2d0a954a897109dba8cc64afddccc3491cea
Freizy <freizykenbrook@gmail.com>
2022-12-23 12:13:09 -0800

    3 redo

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,4 @@
  * print_numbers - prints out numbers from 0 - 9
  * Return: void
  */
-void print_numbers(void)
-{
-	char i;
 
-	for (i = '0', i < '10', i++)
-	{
-		_putchar (i);
-	}
-	_putchar ('\n');
-}
