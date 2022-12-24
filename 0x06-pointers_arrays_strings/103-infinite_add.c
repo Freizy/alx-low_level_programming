5c2a0dcec73bdc1192f5ad9954f9a9bd605c6e24
Freizy <freizykenbrook@gmail.com>
2022-12-23 20:31:20 -0800

    11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,9 +4,9 @@
  * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
- * @r: buffer 
+ * @r: buffer
  * @size_r: buffer size
- * Return: 0
+ * Retrn: 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
