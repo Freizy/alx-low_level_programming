2e4e9240863515c11eefd79d044b0cb5969d67b6
Freizy <freizykenbrook@gmail.com>
2022-12-23 20:45:11 -0800

    12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_line - prints a buffer
+ * print_line - prints bytes of buffer
  * @c: buffer to  be printed
  * @s: bytes of buffer to print
  * @l: lines of buffer to print
@@ -29,4 +29,31 @@ void print_line(char *c, int s, int l)
 		else
 			putchar('.');
 	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
