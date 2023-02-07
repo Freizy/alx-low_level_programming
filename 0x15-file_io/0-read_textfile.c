0b434494d691b55cbe4ae1221032f1eebe93df25
Freizy <61235054+Freizy@users.noreply.github.com>
2023-02-07 05:06:01 +0000

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,32 +10,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	ssize_t fd, r, w;
 	char *buffer;
-
-	if (filename == NULL)
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 		return (0);
 	}
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		return (0);
-	}
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
+	r = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
 	free(buffer);
-	close(o);
-	
+	close(fd);
 	return (w);
 }
