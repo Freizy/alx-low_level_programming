24cef1e3828a905e022309e814e3068ce34b5d0f
Freizy <freizykenbrook@gmail.com>
2023-02-06 20:08:00 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @filename: Pointer to the file name.
  * @letters: The #letters the function reads and prints.
  * Return: The actual number of letters it could read and print.
- * 	   0 if the filename is NULL || write fails.
+ * 0 if the filename is NULL || write fails.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -15,13 +15,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (0);	
+		return (0);
 	}
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		return (0);	
+		return (0);
 	}
 
 	o = open(filename, O_RDONLY);
@@ -31,10 +31,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		return (0);	
+		return (0);
 	}
 
 	free(buffer);
 	close(o);
 	return (w);
-} 
+}
