8216b637fb0d7eb62677262352894975dbe32eed
Freizy <freizykenbrook@gmail.com>
2023-02-14 05:52:48 -0800

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/* _memcpy - function copies @n bytes from memory area @src
+/*
+ * _memcpy - function copies @n bytes from memory area @src
  * to memory area @dest
  * area src to memory area dest
  * @n: bytes of data
